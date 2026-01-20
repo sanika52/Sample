1ad0e70b41278be5793f6ed0fa3adcd6829d713f
SamruddhiKalbhor <samruddhikalbhor19@gmail.com>
2026-01-20 15:46:30 +0530

    fix bugs

--- Add.java
+++ Add.java
@@ -1,8 +1,7 @@
-package Sample;
 
 import java.util.Scanner;
 
-public class Add {
+public class Add{
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
