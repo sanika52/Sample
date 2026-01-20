365c936e86c72b9e4b72c19882dd9c49bce89d19
Sanika Kolhe <sanikakolhe666@gmail.com>
2026-01-20 15:44:28 +0530

    Modified Sub file

--- Sub.java
+++ Sub.java
@@ -1,1 +1,19 @@
-//code
+import java.util.Scanner;
+
+public class Sub{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        int result = a - b;
+
+        System.out.println("Subtraction result: " + result);
+
+        sc.close();
+    }
+}
